4b6b1c38144053b6839818bfd335ff517ae29c24
s27058 <s27058@pjwstk.edu.pl>
2024-03-18 09:20:25 +0100

    Modyfikacja 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,8 @@ public class Program
     public static void Main(string[] args)
     {
         var a = 1;
+        var b = 2;
         Console.WriteLine(a);
+        Console.WriteLine(a+b);
     }
 }
