0e4c0e1d626b05fe795c39ddb61c0f9ab66e4859
s27058 <s27058@pjwstk.edu.pl>
2024-03-18 09:27:24 +0100

    Added CalculateAverage method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,14 @@
-﻿namespace ConsoleApp1;
+﻿
+var a = 1;
+var b = 2;
+var c = 3;
+Console.WriteLine(a);
+Console.WriteLine(a+b);
+Console.WriteLine(a+b*c);
 
-public class Program
+return;
+
+static double CalculateAverage(int[] arr)
 {
-    public static void Main(string[] args)
-    {
-        var a = 1;
-        var b = 2;
-        var c = 3;
-        Console.WriteLine(a);
-        Console.WriteLine(a+b);
-        Console.WriteLine(a+b*c);
-    }
+    return arr.Average();
 }
